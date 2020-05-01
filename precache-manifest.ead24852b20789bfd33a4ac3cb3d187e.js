db69863ad203452f13e99f6d76f5255a1540ea71
sosibona <empty.tara@gmail.com>
2020-05-01 16:50:46 +0300

    Updates

---
+++ precache-manifest.ead24852b20789bfd33a4ac3cb3d187e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75958e351f64b1754cfee05cb30851d",
+    "url": "/sosibona/todo-react.git/index.html"
+  },
+  {
+    "revision": "9adbd28b94e3234de55b",
+    "url": "/sosibona/todo-react.git/static/css/main.f63d940a.chunk.css"
+  },
+  {
+    "revision": "b28cf24d8f0890d0f878",
+    "url": "/sosibona/todo-react.git/static/js/2.e8adc7a4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sosibona/todo-react.git/static/js/2.e8adc7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9adbd28b94e3234de55b",
+    "url": "/sosibona/todo-react.git/static/js/main.fde7b2c7.chunk.js"
+  },
+  {
+    "revision": "94d95afab7dc5843e900",
+    "url": "/sosibona/todo-react.git/static/js/runtime-main.9aa29354.js"
+  }
+]);
